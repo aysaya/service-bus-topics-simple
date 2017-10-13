811fde185beb99da22635bb5cf8f32d6a467362e
aysaya <anastacio.rodrigo@gmail.com>
2017-10-12 22:54:14 -0700

    added init subscribers (notification and pricing service)

---
+++ src/Notification/Controllers/NotificationsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Notification.Controllers
+{
+    [Route("api/[controller]")]
+    public class NotificationsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ src/Pricing/Controllers/PricesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pricing.Controllers
+{
+    [Route("api/[controller]")]
+    public class PricesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
